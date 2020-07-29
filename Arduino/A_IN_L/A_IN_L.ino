786a049e3db3e56fc99eef2b5fb21101ff35b14c
SungyongHong <ricky1303@naver.com>
2020-07-29 10:54:48 +0900

    아두이노코드 재수정 1차

--- Arduino/A_IN_L/A_IN_L.ino
+++ Arduino/A_IN_L/A_IN_L.ino
@@ -1,4 +1,4 @@
-// 입구 왼쪽 
+// 입구 왼쪽 (도면 오른쪽)
 #include<Servo.h>
 
 void setup() {
@@ -6,33 +6,41 @@ void setup() {
   int anglePort = 5;// 서브모터 포트번호 5번으로 통일
   int ir_getH = 6;// 1차 적외선 입력(받는 적외선 신호) 2번이랑 세트
   int ir_getT = 3;// 2차 적외선 입력(받는 적외선 신호) 7번이랑 세트
+  int R_openL = 4;// 라즈베리에서 보내는 open 신호 포트
+  int R_closeL = 3;// 라즈베리에서 보내는 close 신호 포트
   Servo doorUp; // 위서브모터 이름
   Servo doorDown; // 아래서브모터 이름
   doorUp.attach(anglePort); // 위서브모터와 연결
   doorDown.attach(anglePort); // 아래서브모터와 연결
-  pinMode(ir_getH, INPUT); // 오른쪽 2차 적외선 입력 포트
+  pinMode(ir_getH, INPUT); // 오른쪽 1차 적외선 입력 포트
   pinMode(ir_getT, INPUT); // 오른쪽 2차 적외선 입력 포트
+
+  digitalWrite(ir_getH, HIGH) // 1차 적외선센서를 초기화 시킨다
+  digitalWrite(ir_getT, HIGH) // 2차 적외선센서를 초기화 시킨다
 }
 
 void loop() {
 
-  if( digitalRead() == HIGH ) // 라즈베리에서 신호가 오면
+  if( digitalRead(ir_getH) == LOW ) // 오른쪽 1차 적외선에서 신호가 끊기면
   {
-    open(); // 문을 90도 꺽여서 문을 연다
+
+    if( digitalRead(R_openL) == HIGH) // 라즈베리에서 문열라는 신호가 오면
+    {
+      open(); // 문을 90도 꺽어서 문을 연다
+    }
   }
 
-  if( digitalRead() == HIGH ) // 2차 적외선 센서에서 신호가 인식이 안되면
+  if( digitalRead(ir_getT) == LOW ) // 2차 적외선 센서에서 신호가 인식이 안되면
   {
       close(); // 문을 - 90도 꺽어서 문을 닫는다
-      digitalRead() == LOW // 1차 적외선센서를 초기화 시킨다
   }
-
-  digitalRead() == LOW // 2차 적외선 센서를 초기화 시킨다
-      
+  
+  digitalWrite(ir_getH, HIGH) // 1차 적외선센서를 초기화 시킨다
+  digitalWrite(ir_getT, HIGH) // 2차 적외선 센서를 초기화 시킨다
 }
 
 void open (){ // 문  여는 함수
-  for(angle = 0; angle < 90; angle++)
+  for(int angle = 0; angle < 90; angle++)
   {
     doorUp.write(angle);
     doorDown.write(angle);
@@ -41,7 +49,7 @@ void open (){ // 문  여는 함수
 }
 
 void close (){ // 문 닫는 함수
-  for(angle = 0; angle < 90; angle++)
+  for(int angle = 90; angle > 0; angle--)
   {
     doorUp.write(angle);
     doorDown.write(angle);
--- Arduino/A_IN_R/A_IN_R.ino
+++ Arduino/A_IN_R/A_IN_R.ino
@@ -1,4 +1,4 @@
-// 입구 오른쪽 
+// 입구 오른쪽 (도면 왼쪽)
 #include<Servo.h> 
 
 void setup() {
@@ -6,38 +6,43 @@ void setup() {
   int anglePort = 5;// 서브모터 포트번호 5번으로 통일
   int ir_sendH = 2;// 1차 적외선 출력(보내는 적외선 신호) 6번이랑 세트
   int ir_sendT = 7;// 2차 적외선 출력(보내는 적외선 신호) 3번이랑 세트
+  int R_openR = 8;// 라즈베리에서 보내는 open 신호 포트
+  int R_closeR = 9;// 라즈베리에서 보내는 close 신호 포트
+  int A_R = 10;// 라즈베리랑 아두이노 연결하는 포트 10번
   Servo doorUp; // 위서브모터 이름
   Servo doorDown; // 아래서브모터 이름
   doorUp.attach(anglePort); // 위서브모터와 연결
   doorDown.attach(anglePort); // 아래서브모터와 연결
   pinMode(ir_sendH, OUTPUT); // 왼쪽 1차 적외선 출력 포트
   pinMode(ir_sendT, OUTPUT); // 왼쪽 2차 적외선 출력 포트
+
+  digitalWrite(ir_sendH, HIGH) // 1차 적외선센서를 초기화 시킨다
+  digitalWrite(ir_sendT, HIGH) // 2차 적외선 센서를 초기화 시킨다
 }
 
 void loop() {
 
-  if() // 1차 적외선 센서에서 신호가 인식이 안되면
+  if( digitalRead(ir_sendH == LOW) // 1차 적외선 센서에서 신호가 인식이 안되면
   {
-    digitalWrite( , HIGH);
-    // RFID에 신호를 보내서 작동시키고
-    if( digitalRead() == HIGH ) // 라즈베리에서 신호가 오면
+    digitalWrite( A_R, HIGH); // RFID에 신호를 보내서 작동시키고
+    
+    if( digitalRead(R_openR) == HIGH ) // 라즈베리에서 문 열라는신호가 오면
     {
       open(); // 문을 90도 꺽여서 문을 연다
     }
-    
-    if( digitalRead() == HIGH ) // 2차 적외선 센서에서 신호가 인식이 되면
-    {
-      close(); // 문을 -90도 꺽어서 문을 닫는다
-      digitalRead() == LOW // 1차 적외선센서를 초기화 시킨다
-    }
-    
-    digitalRead() == LOW // 2차 적외선센서를 초기화 시킨다
   }
-  
+
+  if( digitalRead(ir_sendT == LOW) // 2차 적외선 센서에서 신호가 인식이 안되면
+  {
+    close(); // 문을 - 90도 꺽어서 문을 닫는다
+  }
+
+  digitalWrite(ir_sendH, HIGH) // 1차 적외선센서를 초기화 시킨다
+  digitalWrite(ir_sendT, HIGH) // 2차 적외선 센서를 초기화 시킨다
 }
 
 void open (){ // 문  여는 함수
-  for(angle = 0; angle < 90; angle++)
+  for(int angle = 0; angle < 90; angle++)
   {
     doorUp.write(angle);
     doorDown.write(angle);
@@ -46,7 +51,7 @@ void open (){ // 문  여는 함수
 }
 
 void close (){ // 문 닫는 함수
-  for(angle = 0; angle < 90; angle++)
+  for(int angle = 90; angle > 0; angle--)
   {
     doorUp.write(angle);
     doorDown.write(angle);
