3b9c730685fa3eba6ab21077d381f13ab86afed5
jieun jang <ji970322@naver.com>
2020-08-28 20:58:00 +0900

    change param of returnbook.c

--- MySql/BookReturn.c
+++ MySql/BookReturn.c
@@ -47,7 +47,7 @@ int main(int argc, char** argv) {
     printf("main---\n");
 
     //int temp = mysql_test();
-    int temp = ReturnBook(331, 367);
+    int temp = ReturnBook(mem_rfid, book_rfid);
 
     return 0;
 }
