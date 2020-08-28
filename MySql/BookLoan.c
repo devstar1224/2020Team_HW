8dcadea7a5d45a0c3f2920b7976917c6229c3b7c
jieun jang <ji970322@naver.com>
2020-08-28 20:17:44 +0900

    update rfid

--- MySql/BookLoan.c
+++ MySql/BookLoan.c
@@ -6,9 +6,12 @@ MYSQL* conn;
 MYSQL_RES* res;
 MYSQL_ROW row;
 int query_stat;
+int mem_rfid, book_rfid;
 
-int LoanBook() {
+int LoanBook(int mem_rfid, int book_rfid) {
     conn = mysql_init(NULL);
+    
+
 
     if (!conn) {
         printf("error");
@@ -25,15 +28,13 @@ int LoanBook() {
     else {
         printf("connect error!\n");
     }
-    char mem_rfid[] = "90635800";
-    char book_rfid[] = "90633F02";
     char query[500];
 
-    sprintf(query, "insert into book_loan(mem_id, id_num) values((select mem_id from member where rfid = '%s'),(select id_num from book_info where rfid = '%s'))" , mem_rfid, book_rfid);
+    sprintf(query, "insert into book_loan(mem_id, id_num) values((select mem_id from member where rfid = %d),(select id_num from book_info where rfid = %d))" , mem_rfid, book_rfid);
 
     query_stat = mysql_query(conn, query);
     if (query_stat != 0) {
-        fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
+        fprintf(stderr, "Mysql query error : %s", mysql_error(conn));
         return 1;
     }
     printf("query sucsess\n");
@@ -46,7 +47,7 @@ int main(int argc, char** argv) {
     printf("main---\n");
 
     //int temp = mysql_test();
-    int temp = LoanBook();
+    int temp = LoanBook(mem_rfid, book_rfid);
 
     return 0;
 }
